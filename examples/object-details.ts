8a5d0bd45ad6267f867b3f47e4f8ce25e6d969d9
Stanislav Popov <popstas@gmail.com>
2025-06-05 23:52:22 +0500

    add TODO.md, fix example

--- examples/object-details.ts
+++ examples/object-details.ts
@@ -3,13 +3,13 @@ import { Configuration, ObjectApi } from '../src/generated';
 // Получение деталей объекта "Продажа"
 export async function getObjectDetails() {
   const config = new Configuration({
-    basePath: 'https://your-account.planfix.com/rest',
-    accessToken: 'YOUR_TOKEN',
+    basePath: `https://${process.env.PLANFIX_ACCOUNT}.planfix.com/rest`,
+    accessToken: process.env.PLANFIX_TOKEN,
   });
 
   const api = new ObjectApi(config);
   // 1. Основные поля объекта "Продажа"
-  const object = await api.getObjectById({ id: 'Продажа' });
+  const object = await api.getObjectById({ id: 617 });
   console.log(object);
 
   // 2. Дополнительные поля объекта "Продажа"
